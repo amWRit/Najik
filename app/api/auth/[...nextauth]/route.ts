4c5455d7fbf6af33f42b4eb6775861b8af58fff0
Amrit-LF <amrit.puri@lftechnology.com>
2026-02-04 17:46:10 +0545

    deploy fx

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,7 +4,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 import { prisma } from "@/lib/prisma/client";
 
-const authOptions = {
+export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
